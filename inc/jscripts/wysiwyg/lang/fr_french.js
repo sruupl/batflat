9ee2c6e8fe95e4204bb4b041e08c6b681d5b5d16
Romain Odeval <romain.odeval@protonmail.com>
2019-12-17 13:19:30 +0100

    Create/update/fix french language

---
+++ inc/jscripts/wysiwyg/lang/fr_french.js
@@ -0,0 +1,145 @@
+(function($) {
+  $.extend($.summernote.lang, {
+    'fr_french': {
+      font: {
+        bold: 'Gras',
+        italic: 'Italique',
+        underline: 'Souligné',
+        clear: 'Retirer le style',
+        height: 'Hauteur',
+        name: 'Nom',
+        strikethrough: 'Barré',
+        size: 'Taille'
+      },
+      image: {
+        image: 'Image',
+        insert: 'Insérer',
+        resizeFull: 'Redimensionner à 100%',
+        resizeHalf: 'Redimensionner à 50%',
+        resizeQuarter: 'Redimensionner à 25%',
+        floatLeft: 'Flottant à gauche',
+        floatRight: 'Flottant à droite',
+        floatNone: 'Pas de flottant',
+        shapeRounded: 'Forme arrondie',
+        shapeCircle: 'Forme cirulaire',
+        shapeThumbnail: 'Forme Miniature',
+        shapeNone: 'Aucune forme',
+        dragImageHere: 'Déplacez l\'image ici',
+        dropImage: 'Déposez l\'image ici',
+        selectFromFiles: 'Sélectionner le fichier',
+        maximumFileSize: 'Limite de taille de fichier',
+        maximumFileSizeError: 'Le fichier est trop lourd.',
+        url: 'URL',
+        remove: 'Supprimer'
+      },
+      video: {
+        video: 'Vidéo',
+        videoLink: 'Lien de la vidéo',
+        insert: 'Insérer',
+        url: 'Adresse de la vidéo',
+        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion)'
+      },
+      link: {
+        link: 'Lien',
+        insert: 'Insérer',
+        unlink: 'Retirer le lien',
+        edit: 'Modifier',
+        textToDisplay: 'Texte à afficher',
+        url: 'URL',
+        openInNewWindow: 'Ouvrir dans une nouvelle fenêtre'
+      },
+      table: {
+        table: 'Tableau'
+      },
+      hr: {
+        insert: 'Insérer une ligne horizontale'
+      },
+      style: {
+        style: 'Style',
+        p: 'Paragraphe',
+        blockquote: 'Citation',
+        pre: 'Code',
+        h1: 'Titre 1',
+        h2: 'Titre 2',
+        h3: 'Titre 3',
+        h4: 'Titre 4',
+        h5: 'Titre 5',
+        h6: 'Titre 6'
+      },
+      lists: {
+        unordered: 'Liste non ordonnée',
+        ordered: 'Liste ordonnée'
+      },
+      options: {
+        help: 'Aide',
+        fullscreen: 'Plein écran',
+        codeview: 'Voir le code'
+      },
+      paragraph: {
+        paragraph: 'Paragraphe',
+        outdent: 'Réduire le retrait',
+        indent: 'Augmenter le retrait',
+        left: 'Aligner à gauche',
+        center: 'Aligner au centre',
+        right: 'Aligner à droite',
+        justify: 'Justifier'
+      },
+      color: {
+        recent: 'Dernière couleur utilisée',
+        more: 'Autres couleurs',
+        background: 'Couleur d\'arrière-plan',
+        foreground: 'Couleur',
+        transparent: 'Transparent',
+        setTransparent: 'Transparent',
+        reset: 'Remise à zéro',
+        resetToDefault: 'Réinitialiser les couleurs'
+      },
+      shortcut: {
+        shortcuts: 'Raccourcis clavier',
+        close: 'Fermer',
+        textFormatting: 'Formatage du texte',
+        action: 'Action',
+        paragraphFormatting: 'Formatage des paragraphes',
+        documentStyle: 'Style du document',
+        extraKeys: 'Touches supplémentaires'
+      },
+      help: {
+        'insertParagraph': 'Insérer un paragraphe',
+        'undo': 'Undoes the last command',
+        'redo': 'Redoes the last command',
+        'tab': 'Tabulation',
+        'untab': 'Untab',
+        'bold': 'Set a bold style',
+        'italic': 'Set a italic style',
+        'underline': 'Set a underline style',
+        'strikethrough': 'Set a strikethrough style',
+        'removeFormat': 'Clean a style',
+        'justifyLeft': 'Set left align',
+        'justifyCenter': 'Set center align',
+        'justifyRight': 'Set right align',
+        'justifyFull': 'Set full align',
+        'insertUnorderedList': 'Toggle unordered list',
+        'insertOrderedList': 'Toggle ordered list',
+        'outdent': 'Outdent on current paragraph',
+        'indent': 'Indent on current paragraph',
+        'formatPara': 'Change current block\'s format as a paragraph(P tag)',
+        'formatH1': 'Change current block\'s format as H1',
+        'formatH2': 'Change current block\'s format as H2',
+        'formatH3': 'Change current block\'s format as H3',
+        'formatH4': 'Change current block\'s format as H4',
+        'formatH5': 'Change current block\'s format as H5',
+        'formatH6': 'Change current block\'s format as H6',
+        'insertHorizontalRule': 'Insert horizontal rule',
+        'linkDialog.show': 'Show Link Dialog'
+      },
+      history: {
+        undo: 'Annuler',
+        redo: 'Répéter'
+      },
+      specialChar: {
+        specialChar: 'CARACTERES SPECIAUX',
+        select: 'Sélectionner une caractère spécial'
+      }
+    }
+  });
+})(jQuery);

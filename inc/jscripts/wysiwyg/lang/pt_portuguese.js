53efdce673d455378cc948c5301ccf75fd62f256
Renato Frota <renatofrota@gmail.com>
2020-05-16 07:50:19 -0300

    Portuguese language for wysiwyg editor

---
+++ inc/jscripts/wysiwyg/lang/pt_portuguese.js
@@ -0,0 +1,155 @@
+(function($) {
+  $.extend($.summernote.lang, {
+    'pt_portuguese': {
+      font: {
+        bold: 'Negrito',
+        italic: 'Itálico',
+        underline: 'Sublinhado',
+        clear: 'Limpar formatação',
+        height: 'Altura da linha',
+        name: 'Família da fonte',
+        strikethrough: 'Riscado',
+        superscript: 'Sobrescrito',
+        subscript: 'Subscrito',
+        size: 'Tamanho da fonte'
+      },
+      image: {
+        image: 'Imagem',
+        insert: 'Inserir imagem',
+        resizeFull: 'Tamanho original',
+        resizeHalf: 'Metade do tamanho',
+        resizeQuarter: 'Um quarto do tamanho',
+        floatLeft: 'Flutuar à esquerda',
+        floatRight: 'Flutuar à direita',
+        floatNone: 'Sem flutuação',
+        shapeRounded: 'Forma: arrendondado',
+        shapeCircle: 'Forma: círculo',
+        shapeThumbnail: 'Forma: miniatura',
+        shapeNone: 'Forma: nenhuma',
+        dragImageHere: 'Arraste uma imagem ou texto aqui',
+        dropImage: 'Solte a imagem ou texto',
+        selectFromFiles: 'Escolher um arquivo',
+        maximumFileSize: 'Tamanho máximo permitido',
+        maximumFileSizeError: 'Tamanho máximo excedido',
+        url: 'URL da imagem',
+        remove: 'Remover imagem',
+        original: 'Original'
+      },
+      video: {
+        video: 'Vídeo',
+        videoLink: 'Vídeo de link externo',
+        insert: 'Inserir vídeo',
+        url: 'URL do vídeo',
+        providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion ou Youku)'
+      },
+      link: {
+        link: 'Link',
+        insert: 'Inserir link',
+        unlink: 'Remover link',
+        edit: 'Editar',
+        textToDisplay: 'Texto a ser exibido',
+        url: 'URL do link',
+        openInNewWindow: 'Abrir em nova aba ou janela'
+      },
+      table: {
+        table: 'Tabela',
+        addRowAbove: 'Adicionar linha acima',
+        addRowBelow: 'Adicionar linha abaixo',
+        addColLeft: 'Adicionar coluna à esquerda',
+        addColRight: 'Adicionar coluna à direita',
+        delRow: 'Excluir linha',
+        delCol: 'Excluir coluna',
+        delTable: 'Excluir tabela'
+      },
+      hr: {
+        insert: 'Inserir linha horizontal'
+      },
+      style: {
+        style: 'Estilo',
+        p: 'Normal',
+        blockquote: 'Citação',
+        pre: 'Pré-formatado',
+        h1: 'Título 1',
+        h2: 'Título 2',
+        h3: 'Título 3',
+        h4: 'Título 4',
+        h5: 'Título 5',
+        h6: 'Título 6'
+      },
+      lists: {
+        unordered: 'Lista não numerada',
+        ordered: 'Lista numerada'
+      },
+      options: {
+        help: 'Ajuda',
+        fullscreen: 'Tela cheia',
+        codeview: 'Exibir código HTML'
+      },
+      paragraph: {
+        paragraph: 'Parágrafo',
+        outdent: 'Diminuir recuo',
+        indent: 'Aumentar recuo',
+        left: 'Alinhado à esquerda',
+        center: 'Centralizado',
+        right: 'Alinhado à direita',
+        justify: 'Justificado'
+      },
+      color: {
+        recent: 'Cores recentes',
+        more: 'Mais cores',
+        background: 'Cor de fundo',
+        foreground: 'Cor do primeiro plano',
+        transparent: 'Transparente',
+        setTransparent: 'Definir transparência',
+        reset: 'Restaurar',
+        resetToDefault: 'Restaurar padrão'
+      },
+      shortcut: {
+        shortcuts: 'Atalhos',
+        close: 'Fechar',
+        textFormatting: 'Formatação de texto',
+        action: 'Ação',
+        paragraphFormatting: 'Formatação de parágrafo',
+        documentStyle: 'Estilo do documento',
+        extraKeys: 'Teclas adicionais'
+      },
+      help: {
+        'insertParagraph': 'Insertar parágrafo',
+        'undo': 'Desfazer',
+        'redo': 'Refazer',
+        'tab': 'Tabular',
+        'untab': 'Reverter tabulação',
+        'bold': 'Negrito',
+        'italic': 'Itálico',
+        'underline': 'Sublinhado',
+        'strikethrough': 'Riscar',
+        'removeFormat': 'Limpar formatação',
+        'justifyLeft': 'Alinhar à esquerda',
+        'justifyCenter': 'Centralizar',
+        'justifyRight': 'Alinhar à direita',
+        'justifyFull': 'Justificar',
+        'insertUnorderedList': 'Inserir lista não numerada',
+        'insertOrderedList': 'Inserir lista numerada',
+        'outdent': 'Diminuir recuo',
+        'indent': 'Aumentar recuo',
+        'formatPara': 'Converter bloco atual em parágrafo (tag p)',
+        'formatH1': 'Converter bloco atual em título H1',
+        'formatH2': 'Converter bloco atual em título H2',
+        'formatH3': 'Converter bloco atual em título H3',
+        'formatH4': 'Converter bloco atual em título H4',
+        'formatH5': 'Converter bloco atual em título H5',
+        'formatH6': 'Converter bloco atual em título H6',
+        'insertHorizontalRule': 'Inserir linha horizontal',
+        'linkDialog.show': 'Mostrar configurações do link'
+      },
+      history: {
+        undo: 'Desfazer a última ação',
+        redo: 'Refazer a última ação desfeita'
+      },
+      specialChar: {
+        specialChar: 'CARACTERES ESPECIAIS',
+        select: 'Escolha os caracteres especiais'
+      }
+    }
+  });
+})(jQuery);
